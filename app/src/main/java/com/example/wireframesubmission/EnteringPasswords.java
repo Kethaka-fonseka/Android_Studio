25681149a51e3e3584b7e9d90218b2242e282b97
HP <deelaka.wanted@gmail.com>
2020-08-15 18:01:25 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/wireframesubmission/EnteringPasswords.java
@@ -0,0 +1,14 @@
+package com.example.wireframesubmission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnteringPasswords extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entering_passwords);
+    }
+}
---
+++ app/src/main/java/com/example/wireframesubmission/RegisterAndLogin.java
@@ -0,0 +1,14 @@
+package com.example.wireframesubmission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterAndLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_and_login);
+    }
+}
