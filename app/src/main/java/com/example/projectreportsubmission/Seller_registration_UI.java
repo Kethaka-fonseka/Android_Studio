10c2ea760819e1cf710ec55f30b93318a3cacf53
INURI JAYASINGHE <it19112688@my.sliit.lk>
2020-08-16 22:44:06 +0530

    IT19112688

---
+++ app/src/main/java/com/example/projectreportsubmission/Seller_registration_UI.java
@@ -0,0 +1,14 @@
+package com.example.projectreportsubmission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Seller_registration_UI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_registration__u_i);
+    }
+}
---
+++ app/src/main/java/com/example/projectreportsubmission/Upload_item.java
@@ -0,0 +1,14 @@
+package com.example.projectreportsubmission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Upload_item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_item);
+    }
+}
